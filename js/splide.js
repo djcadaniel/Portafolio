872a540c3b0caf6d7a465251488048cffc3236f0
djcadaniel <djcadaniel@gmail.com>
2021-07-20 17:47:07 -0500

    falta slider

---
+++ js/splide.js
@@ -0,0 +1,32 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '.splide',{
+        type: 'loop',
+        perPage:1,
+        perMove:1,
+    //     height     : '30rem',
+	// cover      : true,
+        // focus: 'center',
+        // height   : '29rem',
+	    // autoWidth: true,
+        rewind : true,
+        autoplay:true,
+        interval:2000,
+        // focus    : 'center',
+        gap: 15,
+        easing: 'ease',
+        arrows: true,
+        pauseOnHover: true,
+        keyboard:true,
+        // direction  : 'ttb',
+        // heightRatio: 0.6,
+        cover: false,
+        breakpoints: {
+            640: {
+                perPage: 2,
+            },
+            480:{
+                perPage:1
+            }
+        }
+    } ).mount();
+} );
