5454874459b8b9e9f2664ff39bc545963d725390
djcadaniel <djcadaniel@gmail.com>
2021-11-02 16:56:05 -0600

    arreglado menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector(".nav__toggle")
 const navMenu = document.querySelector(".nav")
 
+navMenu.classList.remove("nav__ul--visible")
+
 navToggle.addEventListener("click", ()=>{
     navMenu.classList.toggle("nav__ul--visible")
 })
